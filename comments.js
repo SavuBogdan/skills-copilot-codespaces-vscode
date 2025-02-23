3e884fc2e17ddf9d939d5a0d6c7119b991e23b5d
SavuBogdan <37210559+SavuBogdan@users.noreply.github.com>
2025-02-23 21:53:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,19 @@
-// Give me the code to create a webserver no more comments
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+// Create web server
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+// Create server
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
